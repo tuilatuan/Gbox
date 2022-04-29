9da8a0a51e97ae1b62419f2a6087ad8451d51c64
tuilatuan <pham.kietred@gmail.com>
2022-04-29 20:14:05 +0700

    Gbox

---
+++ dest/main.js
@@ -0,0 +1,75 @@
+function backTotop() {
+  let btntotop = document.querySelector(".footer__copy .arrow");
+  btntotop.addEventListener("click", function () {
+    backtoTop();
+  });
+  function backtoTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+}
+backTotop();
+function nav__mobile() {
+  let nav__mobile = document.querySelector(".nav__mobile");
+  let btnmobile = document.querySelector(".btnhiden");
+
+  btnmobile.addEventListener("click", function () {
+    nav__mobile.classList.toggle("active");
+    btnmobile.classList.toggle("active");
+  });
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 766) {
+      btnmobile.classList.remove("active");
+      nav__mobile.classList.remove("active");
+    }
+  });
+}
+nav__mobile();
+function selectstudio() {
+  let dot = document.querySelectorAll(".dotted-item a");
+  let number = document.querySelector(".number p");
+  let current = 0;
+  function removenavactive(item) {
+    item.forEach(function (aitem) {
+      aitem.classList.remove("active");
+    });
+  }
+  dot.forEach(function (item, index) {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      removenavactive(dot);
+      item.classList.add("active");
+      current = index + 1;
+      number.innerHTML = current.toString().padStart(2, "0");
+    });
+  });
+}
+selectstudio();
+function sliderdetail() {
+  var elem = document.querySelector(".slider__items-img");
+  var flkty = new Flickity(elem, {
+    cellAlign: "left",
+    contain: true,
+    wrapAround: true,
+    prevNextButtons: false,
+    fullscreen: true,
+  });
+  var previousButton = document.querySelector(".btnprevnext .prev");
+  previousButton.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    flkty.previous();
+  });
+  var nextButton = document.querySelector(".btnprevnext .next");
+  nextButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    flkty.next();
+  });
+  // var fullscreenbtn = document.querySelector(".fulls");
+  // fullscreenbtn.addEventListener("click", function () {
+  //   flkty.viewFullscreen();
+  // });
+}
+sliderdetail();
